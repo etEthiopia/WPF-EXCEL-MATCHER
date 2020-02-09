69f2bdc7b81388abcf8cede03e2af12449086a76
etEthiopia <daginegussu@gmail.com>
2020-02-09 06:41:19 +0300

    Caliburn is not Setup

---
+++ ETGDGRegistration/Bootstraper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using ETGDGRegistration.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ETGDGRegistration
+{
+    class Bootstraper : BootstrapperBase
+    {
+        public Bootstraper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+
+    }
+}
