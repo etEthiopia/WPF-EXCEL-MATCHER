3e1d321d19c30bb027634b8522f32a28f2c11cae
etEthiopia <daginegussu@gmail.com>
2020-02-09 15:41:49 +0300

    Read Excel Confortably and Made a Constructor for Team Leader and Individual Competetors

--- DagiCaliburn/Models/StudentModel.cs
+++ DagiCaliburn/Models/StudentModel.cs
@@ -14,14 +14,110 @@ namespace DagiCaliburn.Models
         private string _email = "";
         private bool _params = true;
         private string _errors = "";
+        private string _timestamp = "";
+        private int _women = 0;
+        private string _educationalbg = "";
+        private string _shortthoughts = "";
+        private string _teamleader = "";
         private List<StudentModel> _team = new List<StudentModel>();
 
         public string Name { get { return _name; } set { _name = value; } }
         public int Phone { get { return _phone; } set { _phone = value; } }
         public string Email { get { return _email; } set { _email = value; } }
-        public bool Params { get { return _params;  } set { _params = value; } }
+        public bool Params { get { return _params; } set { _params = value; } }
         public string Errors { get { return _errors; } set { _errors = value; } }
+        public string Timestamp { get { return _timestamp; } set { _timestamp = value; } }
+        public int Women { get { return _women; } set { _women = value; } }
+        public string EducationalBG { get { return _educationalbg; } set { _educationalbg = value; } }
+        public string ShortThoughts { get { return _shortthoughts; } set { _shortthoughts = value; } }
+        public string TeamLeader { get { return _teamleader; } set { _teamleader = value; } }
         public List<StudentModel> Team { get { return _team; } set { _team = value; } }
+
+
+        public StudentModel() { }
+
+        public StudentModel(string name, string phone, string email, string timestamp, string women, string edbg, string thoughts)
+        {
+            if(Regex.IsMatch(name.Trim(), @"^[a-zA-z ]+$"))
+            {
+                Console.WriteLine($"Name Added: {name.Trim()}");
+                Name = name.Trim();
+            }
+            else
+            {
+                Params = false;
+                string errors = "Name Parsing Error. " + Errors;
+                Errors = errors;
+                Console.WriteLine($"Name.Length = 0 : {Params} : {Errors}");
+            }
+
+            if (phone.Trim().Contains("09") && phone.Trim().Length == 10 && !phone.Contains('@'))
+            {
+                var isPhone = 0;
+                if (int.TryParse(phone, out isPhone))
+                {
+                    if (isPhone > 0)
+                    {
+                        Phone = isPhone;
+                        Console.WriteLine($"Phone Added: {isPhone}");
+                    }
+                }
+                else
+                {
+                    Params = false;
+                    string errors = "Invalid Phone Number. " + Errors;
+                    Errors = errors;
+                    Console.WriteLine($"Phone Error : {Params} : {Errors}");
+                }
+            }
+            else
+            {
+                Params = false;
+                string errors = "Invalid Phone Number. " + Errors;
+                Errors = errors;
+                Console.WriteLine($"Phone Error : {Params} : {Errors}");
+            }
+
+            
+
+
+
+            if (email.Contains('@') && email.Contains('.') && !email.Trim().Contains(" "))
+            {
+                Email = email.Trim();
+                Console.WriteLine($"Email Added: {email.Trim()}");
+            }
+            else
+            {
+                Params = false;
+                string errors = "Email Parsing Error. " + Errors;
+                Errors = errors;
+                Console.WriteLine($"Email Error : {Params} : {Errors}");
+            }
+
+            if(Regex.IsMatch(women.Trim(), @"^[0-9]+$"))
+            {
+                var isPhone = 0;
+                if (int.TryParse(phone, out isPhone))
+                {
+                    if (isPhone > 0)
+                    {
+                        Women = isPhone;
+                        Console.WriteLine($"Women Added: {isPhone}");
+                    }
+                }
+            }
+
+            else
+            {
+                Console.WriteLine($"Women Error, Set to '0' by default");
+            }
+
+            EducationalBG = edbg;
+            ShortThoughts = thoughts;
+
+            
+        }
 
         public static List<StudentModel> GetStudents(string k, out bool ap)
         {
--- DagiCaliburn/ViewModels/AnalyzeViewModel.cs
+++ DagiCaliburn/ViewModels/AnalyzeViewModel.cs
@@ -33,22 +33,51 @@ namespace DagiCaliburn.ViewModels
         public void ShowDuplicatesBtn()
         {
             if (!FFile.Equals("")) {
+                //WorkBook wb = WorkBook.Load(FFile);
+                //WorkSheet ws = wb.WorkSheets.First();
+                //string cellValue = ws["A1"].ToString();
+                //Console.WriteLine($"A2: {cellValue}");
+                ReadExcel();
+            }
+            //string k = "Dagmawi Negussu, 0937886725. daginegussu@gmail.com" +
+            //    ": Bereket Yohannes, 0912345678, bekijohn@gmail.com";
+            //bool ap;
+            //List<StudentModel> stus = StudentModel.GetStudents(k,out ap);
+            //if (ap)
+            //{
+            //    foreach(StudentModel st in stus)
+            //    {
+            //        Console.WriteLine("$$$ "+ st.Name + " : " + st.Phone + " : " + st.Email + " : " + st.Params + " : " + st.Errors);
+            //    }
+            //}
+        }
+
+        private void ReadExcel()
+        {
+            try
+            {
                 WorkBook wb = WorkBook.Load(FFile);
                 WorkSheet ws = wb.WorkSheets.First();
-                string cellValue = ws["A1"].ToString();
-                Console.WriteLine($"A2: {cellValue}");
-
-            }
-            string k = "Dagmawi Negussu, 0937886725. daginegussu@gmail.com" +
-                ": Bereket Yohannes, 0912345678, bekijohn@gmail.com";
-            bool ap;
-            List<StudentModel> stus = StudentModel.GetStudents(k,out ap);
-            if (ap)
-            {
-                foreach(StudentModel st in stus)
+                foreach (RangeRow row in ws.Rows)
                 {
-                    Console.WriteLine("$$$ "+ st.Name + " : " + st.Phone + " : " + st.Email + " : " + st.Params + " : " + st.Errors);
+                    if (row.First().Value.ToString().Trim().Equals("Timestamp"))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("New Row >>  ");
+                        foreach (IronXL.Cell k in row.ToList())
+                        {
+                            Console.Write($"{k.Value.ToString()} : ");
+                        }
+                        Console.Write("\n");
+                    }
                 }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error : " + e.Message);
             }
         }
     }
