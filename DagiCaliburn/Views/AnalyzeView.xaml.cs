e8d7913e74a5246d842b5ccf7b020bca97a9d205
etEthiopia <daginegussu@gmail.com>
2020-02-09 08:14:17 +0300

    Everything works

---
+++ DagiCaliburn/Views/AnalyzeView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DagiCaliburn.Views
+{
+    /// <summary>
+    /// Interaction logic for AnalyzeView.xaml
+    /// </summary>
+    public partial class AnalyzeView : Page
+    {
+        public AnalyzeView()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_PreviewDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_PreviewDragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_PreviewDragOver(object sender, DragEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ packages/IronXL.Excel.2019.5.2/readme.txt
@@ -0,0 +1,12 @@
+﻿IronXL - The Excel Library for C# and .Net Projects.
+
+Create, Open, Read and Export XLS and XLS files in C# /.NET Projects without using Microsoft.Office.Interop.Excel
+
+Supports: XLS/XLSX/CSV/TSV/JSON and XML
+Platforms: .Net Framework 4.5+, Dot Net Core 2.0+ , Azure, Xamarin, Mobile & Mono
+
+Please visit https://ironsoftware.com/csharp/excel/ for:
+- Code Samples,
+- Support,
+- MSDN object reference,
+- Detailed Community Tutorials.
